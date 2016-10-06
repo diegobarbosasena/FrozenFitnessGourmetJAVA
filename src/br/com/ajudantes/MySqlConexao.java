a2d384604426cb39f339e1ed14f8a948f2490aeb
senadiego <diegosena_93@hotmail.com>
2016-10-06 00:23:20 -0300

    quase conexao com MySql

---
+++ src/br/com/ajudantes/MySqlConexao.java
@@ -0,0 +1,26 @@
+package br.com.ajudantes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MySqlConexao {
+
+	public static Connection ConectarDb(){
+		
+		String ip = "jdbc:mysql://10.107.144.40/contatos";
+		String usuario = "root";
+		String senha = "root" ;
+		
+		Connection c = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");		
+			c = DriverManager.getConnection(ip, usuario, senha);
+			
+		} catch (Exception e) {	
+			e.printStackTrace();
+		}
+
+		return c;	
+	}
+}
--- src/br/com/model/Transportadora.java
+++ src/br/com/model/Transportadora.java
@@ -2,6 +2,51 @@ package br.com.model;
 
 public class Transportadora {
 
+	private int codTransportadora;
+	private String nomeTransportadora;
+	private String emailTransportadora;
+	private String telefoneTransportadora;
+	private String cnpjTransportadora;
+	private String responsavelTransportadora;
+	
+	
+	public int getCodTransportadora() {
+		return codTransportadora;
+	}
+	public void setCodTransportadora(int codTransportadora) {
+		this.codTransportadora = codTransportadora;
+	}
+	public String getNomeTransportadora() {
+		return nomeTransportadora;
+	}
+	public void setNomeTransportadora(String nomeTransportadora) {
+		this.nomeTransportadora = nomeTransportadora;
+	}
+	public String getEmailTransportadora() {
+		return emailTransportadora;
+	}
+	public void setEmailTransportadora(String emailTransportadora) {
+		this.emailTransportadora = emailTransportadora;
+	}
+	public String getTelefoneTransportadora() {
+		return telefoneTransportadora;
+	}
+	public void setTelefoneTransportadora(String telefoneTransportadora) {
+		this.telefoneTransportadora = telefoneTransportadora;
+	}
+	public String getCnpjTransportadora() {
+		return cnpjTransportadora;
+	}
+	public void setCnpjTransportadora(String cnpjTransportadora) {
+		this.cnpjTransportadora = cnpjTransportadora;
+	}
+	public String getResponsavelTransportadora() {
+		return responsavelTransportadora;
+	}
+	public void setResponsavelTransportadora(String responsavelTransportadora) {
+		this.responsavelTransportadora = responsavelTransportadora;
+	}
+	
 	
 	
 }
