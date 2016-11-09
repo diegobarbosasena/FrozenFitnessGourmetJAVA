3b2eb495a687464cd8a10407cc9b76d9acb50032
senadiego <diegosena_93@hotmail.com>
2016-11-08 22:30:35 -0200

    pedido telefone - botao relatorios - grupo radiobuton

--- src/br/com/controller/GraficoVendasController.java
+++ src/br/com/controller/GraficoVendasController.java
@@ -5,47 +5,59 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.XYChart;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
+
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Font;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.BarChart;
 
-public class GraficoVendasController implements Initializable{
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Button;
+
+public class GraficoVendasController implements Initializable {
 	
 	@FXML private AnchorPane anpGrafVendas;
-	
 	@FXML private RadioButton rbSemanal;
 	@FXML private RadioButton rbMensal;
 	@FXML private RadioButton rbTrimestral;
 	@FXML private RadioButton rbPeriodo;
-	
 	@FXML private Label lblDtInicial;
 	@FXML private Label lblDtFim;
-	
 	@FXML private DatePicker dtpInicio;
 	@FXML private DatePicker dtpFim;
-	
-	@FXML private Button btnFiltrar;
-	
 	@FXML private BarChart<String, Number> brcGrafVendas;
 	@FXML private CategoryAxis xEixo;
+	@FXML private Button btnRelatorio;
 	
-
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
-		rbSemanal.setOnAction(s -> preencherGrafico());
+		radioButtonGroup();
+		
+		rbSemanal.setOnAction(s -> preencherGraficoSemanal());
+		rbMensal.setOnAction(m -> preencherGraficoMensal());
+		rbTrimestral.setOnAction(t -> preencherGraficoTrimestral());
+		rbPeriodo.setOnAction(p -> preencherGraficoPeriodo());
+		
 		xEixo.tickLabelFontProperty().set(Font.font(14));
 		brcGrafVendas.setTitle("Gráfico de Vendas");
 	}
-
 	
-	private void preencherGrafico() {
+	public void radioButtonGroup() {
+		final ToggleGroup tgFiltrar = new ToggleGroup();
+		
+		rbMensal.setToggleGroup(tgFiltrar);
+		rbPeriodo.setToggleGroup(tgFiltrar);
+		rbSemanal.setToggleGroup(tgFiltrar);
+		rbTrimestral.setToggleGroup(tgFiltrar);
+	}
+	
+	private void preencherGraficoSemanal() {
 		
 		XYChart.Data<String, Number> perdaPeso = new XYChart.Data<String, Number>("Perda de Peso", 18.156);	
 		XYChart.Data<String, Number> ganhoMassa = new XYChart.Data<String, Number>("Ganho de Massa", 9.115);
@@ -64,8 +76,23 @@ public class GraficoVendasController implements Initializable{
 		totalCatego.getData().add(total);
 		
 		brcGrafVendas.getData().add(totalCatego);
+	}
 	
-		rbSemanal.setDisable(true);
-	}	
+	private void preencherGraficoTrimestral() {
+		
+	}
 
+	private void preencherGraficoMensal() {
+		
+	}
+
+	private void preencherGraficoPeriodo() {
+		
+		dtpInicio.setDisable(false);
+		dtpFim.setDisable(false);
+		lblDtInicial.setDisable(false);
+		lblDtFim.setDisable(false);
+	}
+
+	
 }
--- src/br/com/controller/PedidoTelefoneController.java
+++ src/br/com/controller/PedidoTelefoneController.java
@@ -1,5 +1,82 @@
 package br.com.controller;
 
-public class PedidoTelefoneController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Label;
+
+import javafx.scene.control.ListView;
+
+import javafx.scene.control.ComboBox;
+
+import javafx.scene.layout.AnchorPane;
+
+import javafx.scene.control.RadioButton;
+
+import javafx.scene.control.DatePicker;
+
+public class PedidoTelefoneController implements Initializable {
+	
+	@FXML private AnchorPane anpPedidosTelefone;
+	@FXML private Label lblECliente;
+	@FXML private RadioButton rbSim;
+	@FXML private RadioButton rbNao;
+	@FXML private Label lblListaCliente;
+	@FXML private ComboBox<?> cboCliente;
+	@FXML private Label lblNomeClien;
+	@FXML private TextField txtNomeClien;
+	@FXML private Label lblCpfClien;
+	@FXML private TextField txtCpfClien;
+	@FXML private Label lblDatNacClien;
+	@FXML private DatePicker dpDtNasc;
+	@FXML private Label lblPeso;
+	@FXML private TextField txtPeso;
+	@FXML private Label lblAltura;
+	@FXML private TextField txtAltura;
+	@FXML private Label lblTelefone;
+	@FXML private TextField txtTel;
+	@FXML private Label lblCel;
+	@FXML private TextField txtCel;
+	@FXML private Label lblEmail;
+	@FXML private TextField txtEmail;
+	@FXML private Label lblSexo;
+	@FXML private RadioButton rbSexoM;
+	@FXML private RadioButton rbSexoF;
+	@FXML private Label lblCpfDoClien;
+	@FXML private ComboBox<?> cboCpfClien;
+	@FXML private Label lblPrato;
+	@FXML private ComboBox<?> cboPrato;
+	@FXML private Label lblQnt;
+	@FXML private ComboBox<?> cboQnt;
+	@FXML private Button btnAdicionar;
+	@FXML private Button btnExcluir;
+	@FXML private Label lblItens;
+	@FXML private ListView<?> lsvItensPedido;
+	@FXML private Button btnFinalizar;
+	@FXML private Button btnCancelar;
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		grupoRadioButton();	
+	}
+
+	public void grupoRadioButton() {
+		final ToggleGroup tgSN = new ToggleGroup();
+		final ToggleGroup tgSx = new ToggleGroup();
+		
+		rbSim.setToggleGroup(tgSN);
+		rbNao.setToggleGroup(tgSN);
+		
+		rbSexoM.setToggleGroup(tgSx);
+		rbSexoF.setToggleGroup(tgSx);
+	}
 
 }
